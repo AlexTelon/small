befa0d8acf9d5b36125a0a5744caabd4465de001
Alex Telon <alex.telon@gmail.com>
2023-07-11 14:12:37 +0200

    minor refactoring

--- chat/chat.py
+++ chat/chat.py
@@ -4,16 +4,15 @@ from functools import partial
 
 db = partial(open, db_path+room)
 
-def logs():
-    try:
-        print('\n'.join(f"{user}: {msg}" for user, msg in map(eval, db('r'))))
-    except FileNotFoundError:
-        print()
-
 match sys.argv[1:]:
     case ["user", user]:     print(f"{user=}")
     case ["join", room]:     print(f"{room=}")
-    case ["write", *args]:   db('a').write(f"'{user}', '{' '.join(args)}'\n")
-    case _:                  logs()
+    case ["write", *args]:   db('a').write(f"{user=}; msg='{' '.join(args)}'\n")
+    case _:
+        try:
+            for line in db():
+                exec(line+'print(f"{user}: {msg}")')
+        except FileNotFoundError:
+            print()
 
 open('user.py','w').write(f"{room=}\n{user=}\n{db_path=}")
