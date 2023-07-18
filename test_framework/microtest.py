026373efe8b55777cbf6a969c2ef960681c289d6
Alex Telon <alex.telon@gmail.com>
2023-07-18 21:46:09 +0200

    Code is shorter but harder to read, success!
    But i successfully removed the second instance of '.py'!
    
    I feelt it should be unecessary to say the same thing twice.
    To check that it has '.py' and then remove it.
    
    So this version does both in one but is harder to read for no real
    benefit so not production code..

--- test_framework/microtest.py
+++ test_framework/microtest.py
@@ -7,16 +7,15 @@ def get_tests(module):
     return [f for name, f in inspect.getmembers(module, inspect.isfunction) if name.startswith('test_')]
 
 def module(file):
-    if not file.endswith('.py'):
-        return None
-    module_name = os.path.basename(file).replace('.py', '')
-    spec = importlib.util.spec_from_file_location(module_name, file)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    return module
+    match os.path.basename(file).split('.py'):
+        case (module_name, ''):
+            spec = importlib.util.spec_from_file_location(module_name, file)
+            module = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(module)
+            return module
 
 if __name__ == '__main__':
-    modules = list(map(module, set(sys.argv[1:])))
+    modules = map(module, set(sys.argv[1:]))
     tests = (test for module in modules for test in get_tests(module))
     for test in tests:
         test()
